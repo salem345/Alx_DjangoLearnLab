048c7648d58e6352de143d8bccb88e63a15ee75c
Salem <salemmahmoud345@gmail.com>
2025-08-23 18:22:38 +0300

    that's it

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+
+
+
+    def __str__(self):
+        return self.title
 
 # Register your models here.
