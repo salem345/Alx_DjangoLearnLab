6a57c9b6f32e06f7c1e74e7cf178970aceea8254
Salem <salemmahmoud345@gmail.com>
2025-08-24 17:20:22 +0300

    finally

--- advanced_features_and_security/relationship_app/urls.py
+++ advanced_features_and_security/relationship_app/urls.py
@@ -22,9 +22,9 @@ from django.urls import path
 from .views import register_view, login_view, logout_view
 
 urlpatterns = [
-    views.LoginView.as_view(template_name="relationship_app/login.html"),
-    views.RegisterView.as_view(template_name="relationship_app/register.html"),
-    views.LogoutView.as_view(template_name="relationship_app/logout.html"),
+    views.login_view.as_view(template_name="relationship_app/login.html"),
+    views.register_view.as_view(template_name="relationship_app/register.html"),
+    views.logout_view.as_view(template_name="relationship_app/logout.html"),
     path("register/", views.register_view, name="register"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
