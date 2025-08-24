29174f53387c684378d46481b017061469f0f1f7
Salem <salemmahmoud345@gmail.com>
2025-08-24 14:38:47 +0300

    let's go

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("books/", views.list_books, name="list_books"),
     path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
 ]
-
+from django.contrib.auth.forms import UserCreationForm
 from django.urls import path
 from .views import register_view, login_view, logout_view
 
