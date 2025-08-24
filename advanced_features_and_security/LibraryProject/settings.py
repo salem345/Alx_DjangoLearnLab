1f95f6bc0202dba817257615ad9c5d70b1a3f57d
Salem <salemmahmoud345@gmail.com>
2025-08-24 15:17:50 +0300

    let's go

--- advanced_features_and_security/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/settings.py
@@ -29,14 +29,18 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
+AUTH_USER_MODEL = 'accounts.CustomUser'
 INSTALLED_APPS = [
+    # Django apps...
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Your apps
+    'accounts',
 ]
 
 MIDDLEWARE = [
