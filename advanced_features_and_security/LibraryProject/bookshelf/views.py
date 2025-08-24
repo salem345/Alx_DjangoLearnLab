32beae383829b739f5fd522a89f19f5f0f49ee8a
Salem <salemmahmoud345@gmail.com>
2025-08-24 16:00:52 +0300

    let's go

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -5,6 +5,10 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import permission_required, login_required
 from .models import Book
 
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, "bookshelf/book_list.html", {"books": books})
+
 # -------- View: عرض الكتب (requires can_view) --------
 @permission_required('bookshelf.can_view', raise_exception=True)
 def list_books(request):
