6a75e6d1ee8d0d052138723395948563ce7d4e41
Salem <salemmahmoud345@gmail.com>
2025-08-27 23:31:51 +0300

    yaaraab

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -7,3 +7,5 @@ urlpatterns = [
      path('follow/<int:user_id>/', follow_user, name='follow-user'),
     path('unfollow/<int:user_id>/', unfollow_user, name='unfollow-user'),
 ]
+
+["unfollow/<int:user_id>/", "follow/<int:user_id>"]
